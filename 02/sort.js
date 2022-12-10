4b29772ffa519a182848eefaa6ef8367ff20cfe1
enitechajin <purenarim@hanmail.net>
2022-12-10 20:43:52 +0900

    it is too hard to study sort

---
+++ 02/sort.js
@@ -0,0 +1,10 @@
+//bubble sort
+let arr = [7,4,5,1,3];
+//let bubble = [];
+//let cnt = 0;
+let first = 0;
+for(let i = 0; i < arr.length; i++) {7
+    console.log("bubble", arr[i]);
+    first = arr[0];
+}
+
