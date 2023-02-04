66848b1bb855f644d86777282ae26fb0650a2bf9
hajiny <betterthanhajin@icolud.com>
2023-02-04 17:46:34 +0900

    test

---
+++ 04/vue-typescript/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <base-header />
+    </div>
+    <div class="row">input</div>
+    <div class="row">
+      <router-view />
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+import BaseHeader from "@/components/header.vue";
+</script>
+<style lang="scss">
+#app {
+  padding: 50px 0;
+  .row + .row {
+    margin-top: 15px;
+  }
+}
+</style>
---
+++ 04/vue-typescript/src/components/header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+    <h1>TODO LIST</h1>
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="#">Home</a></li>
+        <li class="breadcrumb-item"><a href="#">Library</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Data</li>
+      </ol>
+    </nav>
+  </header>
+</template>
+<script></script>
+<style lang="scss"></style>
